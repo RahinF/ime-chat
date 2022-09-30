66c51ed8bdc35281b83742d0a3e74731edb6054a
RahinF <s3297352@student.rmit.edu.au>
2022-09-30 18:28:03 +1000

    add home path

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import Register from "./pages/Register";
 const App = () => {
   return (
     <Routes>
-      <Route element={<RequireAuth />}>
-        <Route index element={<Home />} />
+      <Route path="/">
+        <Route element={<RequireAuth />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Route>
-      <Route path="login" element={<Login />} />
-      <Route path="register" element={<Register />} />
     </Routes>
   );
 };
