3bad890596c6558b2a4b7618ac2938e0848c871a
RahinF <s3297352@student.rmit.edu.au>
2022-10-01 12:10:01 +1000

    add sidebar chats functionality

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,11 +1,62 @@
-import Input from "./Input";
-import Messages from "./Messages";
+import { doc, DocumentData, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import Placeholder from "../assets/avatar_placeholder.png";
+import { db } from "../firebase";
+import useUserStore from "../hooks/useUserStore";
 
-const Chat = () => {
+interface ChatType {
+  date: {
+    nanoseconds: number;
+    seconds: number;
+  };
+
+  uid: string;
+}
+
+const Chat = ({ chat }: { chat: ChatType }) => {
+  const [user, setUser] = useState<DocumentData | null>(null);
+
+  const { setChatUser } = useUserStore();
+
+  useEffect(() => {
+    if (!chat.uid) return;
+    const unsub = onSnapshot(doc(db, "users", chat.uid), (doc) => {
+      if (doc.exists()) {
+        setUser(doc.data());
+      }
+    });
+
+    return () => {
+      unsub();
+    };
+  }, [chat.uid]);
+
+  const handleSelect = () => {
+    if (user) {
+      const { displayName, photoURL, uid } = user;
+      setChatUser({ displayName, photoURL, uid });
+    }
+  };
+
   return (
-    <div className="flex basis-full flex-col bg-white">
-      <Messages />
-      <Input />
+    <div
+      className="flex cursor-pointer gap-2 p-4 hover:bg-blue-500 hover:text-white"
+      onClick={handleSelect}
+    >
+      <img
+        className="h-12 w-12 shrink-0 rounded-full object-cover"
+        src={user?.photoURL || Placeholder}
+        alt="avatar"
+      />
+      <div>
+        <div className="font-bold">{user?.displayName}</div>
+        <div className="w-40 truncate">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          praesentium dolore nesciunt provident placeat alias temporibus!
+          Dolorem minima, aliquid provident inventore sed ex quis maiores
+          eveniet dolores similique, alias qui.
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -1,23 +1,44 @@
+import { doc, DocumentData, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+import useUserStore from "../hooks/useUserStore";
+import Chat from "./Chat";
+
+interface ChatType {
+  date: {
+    nanoseconds: number;
+    seconds: number;
+  };
+
+  uid: string;
+}
+
 const Chats = () => {
+  const [chats, setChats] = useState<ChatType[] | []>([]);
+  const { currentUser } = useUserStore();
+
+  useEffect(() => {
+    if (!currentUser) return;
+    const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+      if (doc.exists()) {
+        const array = Object.values(doc.data() as DocumentData);
+        const chats = array as ChatType[];
+        setChats(chats);
+      }
+    });
+
+    return () => {
+      unsub();
+    };
+  }, [currentUser]);
+
   return (
     <div className="flex flex-col divide-y overflow-y-auto">
-      {[...Array(20)].map((element, index) => (
-        <div
-          key={index}
-          className="flex cursor-pointer gap-2 p-4 hover:bg-blue-500 hover:text-white"
-        >
-          <div className="h-12 w-12 shrink-0 rounded-full bg-blue-300" />
-          <div>
-            <div className="font-bold">John Smith</div>
-            <div className="truncate max-w-xs">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-              praesentium dolore nesciunt provident placeat alias temporibus!
-              Dolorem minima, aliquid provident inventore sed ex quis maiores
-              eveniet dolores similique, alias qui.
-            </div>
-          </div>
-        </div>
-      ))}
+      {chats
+        .sort((a, b) => b.date.seconds - a.date.seconds)
+        .map((chat, index) => (
+          <Chat chat={chat} key={index} />
+        ))}
     </div>
   );
 };
